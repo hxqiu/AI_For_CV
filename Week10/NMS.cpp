b597ebc4e93896b30fba608ff94a5643836fe398
HxQiu <qiu.huann@gmail.com>
2019-06-24 10:46:37 +0800

    Update NMS.cpp

--- Week10/NMS.cpp
+++ Week10/NMS.cpp
@@ -6,32 +6,43 @@ using namespace std;
 
 class BoundingBox {
 public:
-	double x;
-	double y;
-	double w;
-	double h;
+	float x;
+	float y;
+	float w;
+	float h;
 
-	double area() {
+	float area() {
 		return w * h;
+	}
+
+	BoundingBox() {
+		x = y = w = h = 0.0;
+	}
+
+	BoundingBox(float x, float y, float w, float h) {
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
 	}
 };
 
-double cal_IoU(BoundingBox& BoxA,
-	           BoundingBox& BoxB) {
-	double intersection_x = min(BoxA.x + BoxA.w, BoxB.x + BoxB.w) - max(BoxA.x, BoxB.x);
-	double intersection_y = min(BoxA.y + BoxA.h, BoxB.y + BoxB.h) - max(BoxA.y, BoxB.y);
+float cal_IoU(BoundingBox& BoxA,
+	BoundingBox& BoxB) {
+	float intersection_x = min(BoxA.x + BoxA.w, BoxB.x + BoxB.w) - max(BoxA.x, BoxB.x);
+	float intersection_y = min(BoxA.y + BoxA.h, BoxB.y + BoxB.h) - max(BoxA.y, BoxB.y);
 	if (intersection_x < 0.0 || intersection_y < 0.0)
 		return 0.0;
-	double area_of_intersection = intersection_x * intersection_y;
-	double area_of_union = BoxA.area + BoxB.area - area_of_intersection;
-	return area_of_intersection/ area_of_union;
+	float area_of_intersection = intersection_x * intersection_y;
+	float area_of_union = BoxA.area() + BoxB.area() - area_of_intersection;
+	return area_of_intersection / area_of_union;
 }
 vector<vector<float>>NMS(vector<vector<float>>lists, float thre) {
 	vector<BoundingBox> B;
-	vector<double> S;
-	vector<vector<float>> ret;
-	vector<double> result_S;
-	for (list : lists) {
+	vector<float> S;
+	vector<BoundingBox> D;
+	vector<float> result_S;
+	for (vector<float> list : lists) {
 		B.push_back(BoundingBox(list[0], list[1], list[2], list[3]));
 		S.push_back(list[4]);
 	}
@@ -41,7 +52,7 @@ vector<vector<float>>NMS(vector<vector<float>>lists, float thre) {
 		int max_element_ind = max_element_it - S.begin();
 		M = B[max_element_ind];
 		D.push_back(M);
-		result_S.push_back(S[max_element_ind])
+		result_S.push_back(S[max_element_ind]);
 		B.erase(B.begin() + max_element_ind);
 		S.erase(S.begin() + max_element_ind);
 		for (int i = 0; i < B.size(); i++) {
@@ -52,42 +63,23 @@ vector<vector<float>>NMS(vector<vector<float>>lists, float thre) {
 			}
 		}
 	}
+	vector<vector<float>> ret;
+	for (int i = 0; i < D.size(); i++) {
+		ret.push_back({ D[i].x, D[i].y, D[i].w, D[i].h, result_S[i] });
+	}
+	return ret;
 }
-void NMS(vector<BoundingBox> &B, 
-		 vector<double> &S,
-		 double threshold,
-	     vector<BoundingBox> &D,
-		 vector<double> &result_S) {
-	BoundingBox M;
-	while (!B.empty()) {
-		auto max_element_it = max_element(S.begin(), S.end());
-		int max_element_ind = max_element_it - S.begin();
-		M = B[max_element_ind];
-		D.push_back(M);
-		B.erase(B.begin() + max_element_ind);
-		for (int i = 0; i < B.size(); i++) {
-			if (cal_IoU(B[i], M) > threshold) {
-				B.erase(B.begin() + i);
-				S.erase(S.begin() + i);
-				--i;
-			}
-		}
-	}	
-}
+
 int main() {
-	BoundingBox b1(254.0, 259.0, 205.0, 205.0);
-	BoundingBox b2(102.0, 189.0, 285.0, 285.0);
-	BoundingBox b3(635.0, 372.0, 463.0, 743.0);
-	BoundingBox b4(643.0, 243.0, 532.0, 634.0);
-	
-	vector<BoundingBox> list_bbox;
-	vector<double> scores = { 0.87, 0.9, 0.72, 0.2 };
-	vector<BoundingBox> result_b;
-	vector<double> result_scores;
-	list_bbox.push_back(b1);
-	list_bbox.push_back(b2);
-	list_bbox.push_back(b3);
-	list_bbox.push_back(b4);
-	NMS(list_bbox, scores, 0.3, result_b, result_scores);
+	vector<float> list1 = { 254.0, 259.0, 205.0, 205.0, 0.87 };
+	vector<float> list2 = { 102.0, 189.0, 285.0, 285.0, 0.9 };
+	vector<float> list3 = { 635.0, 372.0, 463.0, 743.0, 0.72 };
+	vector<float> list4 = { 643.0, 243.0, 532.0, 634.0, 0.2 };
+	vector<vector<float>> lists;
+	lists.push_back(list1);
+	lists.push_back(list2);
+	lists.push_back(list3);
+	lists.push_back(list4);
+	vector<vector<float>> result = NMS(lists, 0.3);
 	return 0;
 }
